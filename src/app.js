99d296db0d1e9ebc97b8ff1ba28e80c3ede0ed96
Aaron Pitarch <aapitarch@gmail.com>
2024-08-22 11:59:54 +0000

    primer commit

--- src/app.js
+++ src/app.js
@@ -2,10 +2,24 @@
 import "bootstrap";
 import "./style.css";
 
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
-
 window.onload = function() {
   //write your code here
-  console.log("Hello Rigo from the console!");
+  let who = ["El perro", "Mi abuela", "El cartero", "Mi pajaro"];
+  let action = [" comió", " pisó", " aplastó", " rompió"];
+  let what = [" mi trabajo", " mi telefono", " el coche"];
+  let when = [
+    " antes de la clase",
+    " cuando dormía",
+    " mientras hacía ejercicio",
+    " durante el desayuno",
+    " mientras rezaba"
+  ];
+
+  let random1 = Math.floor(Math.random() * who.length);
+  let random2 = Math.floor(Math.random() * action.length);
+  let random3 = Math.floor(Math.random() * what.length);
+  let random4 = Math.floor(Math.random() * when.length);
+
+  document.querySelector("#excuse").innerHTML =
+    who[random1] + action[random2] + what[random3] + when[random4];
 };
